b7e4affb74303a6f6cb15f4f5f3f6e5f4d8b5589
YuriyGorodzhiy <yuri_tes@mail.ru>
2021-03-16 02:40:03 +0300

    feat: finish

--- src/index.js
+++ src/index.js
@@ -1,3 +1,32 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  var arr = str.split(''),
+  stack = [],
+  open = [],
+  close = [],
+  closeIndex,
+  openIndex;
+
+  for (var i=0; i<bracketsConfig.length; i++) {
+    open.push(bracketsConfig[i][0]);
+    close.push(bracketsConfig[i][1]);
+  }
+  for ( var i=0; i<arr.length; i++) {
+    openIndex = open.indexOf(arr[i]);
+    if (openIndex !== -1) {
+      stack.push(openIndex);
+      continue;
+    }
+
+    closeIndex = close.indexOf(arr[i]);
+    if (closeIndex !== -1) {
+      openIndex = stack.pop();
+      if (closeIndex !== openIndex) {
+          return false;
+      }
+    }
+  }
+  if (stack.length != 0) {
+    return false;
+  }
+  return true;
 }
